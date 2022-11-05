8e16380c84f1dd18c30a70b7f92b61bd5f86f5f7
Shahriar-Shafi <shahriarshafic@gmail.com>
2022-11-05 18:52:53 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+// if return is empty then it is undefined
+// function add(num1,num2) {
+//     return num1 + num2;
+// }
+
+//Num 2 is not defined as a result it will be undefined
+// function add(num1,num2) {
+//     console.log(num1+num2)
+// }
+
+//Not having the info but searching for it will also be undefined
+// const guy = {name:"Smart dude", phone: 45454}
+// console.log(guy.gf);
+
+// let fun = undefined
+// console.log(fun);
+
+const num = [12,11,15]
+console.log(num[11]);
---
+++ true-false.js
@@ -0,0 +1,11 @@
+const age = 4
+// If the value is 0 it will always be false
+// If the variable is undefined it will also give false as a result
+let name;
+if(name){
+    console.log('Condition is true');
+}
+else{
+    console.log('Condition is false');
+}
+// Falsy values: 0, "" empty string, variable is undefined, variable is null, var is NaN, var decleared false
